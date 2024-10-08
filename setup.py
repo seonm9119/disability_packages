ff4a2d42779a4abd8d1147bd688941368b2b45e9
seonm9119 <seonm9119@gmail.com>
2024-10-08 14:20:47 +0900

    update packages

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='disability',
       url='https://github.com/seonm9119/disability_packages.git',
       author='nami',
       author_email='seonm9119@gmail.com',
-      packages=find_packages(include=['disability']),
+      packages=find_packages(include=['disability', 'organizer']),
       install_requires=['pandas'])
