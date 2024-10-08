292b9af5a60d641b2d16ac71056467332f8c5f43
seonm9119 <seonm9119@gmail.com>
2024-10-08 13:39:02 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='disability',
+      version='0.0.0',
+      description='Communication Project for People with Disabilities',
+      url='https://github.com/seonm9119/disability_packages.git',
+      author='nami',
+      author_email='seonm9119@gmail.com',
+      packages=find_packages(),
+      install_requires=['pandas'])
